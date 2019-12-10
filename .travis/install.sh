e532edf9a68a2e049e6992fe0f1a7bd0a5c6db83
czoido <mrgalleta@gmail.com>
2019-12-10 12:39:05 +0100

    update python ver

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,12 +13,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
 
-pip install conan
+pip install conan --upgrade
+
 conan user
 pip install nose
